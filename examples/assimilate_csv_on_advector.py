dfacf3351ac6c4a5ce225a6520ca35e39f31e0b4
Axel Peytavin <licks92@gmail.com>
2021-05-06 09:26:13 +0000

    Update examples in scripts

--- examples/assimilate_csv_on_advector.py
+++ examples/assimilate_csv_on_advector.py
@@ -1,15 +1,25 @@
 from src.run_assimilator import run_assimilator
+import numpy as np
+
+cell_area = lambda dlon,dlat,lat: dlat * 111 * dlon * 111 * np.cos(lat * np.pi / 180)
+
+GRID_COORDS = (-165, 23, -125, 45)
+GRID_RESOLUTION = 0.5
+
+cells_area = np.array([[cell_area(0.5, 0.5, lat_id * GRID_RESOLUTION + GRID_COORDS[1]) for lat_id in range(44)] for lon_id in range(80)])
 
 if __name__ == "__main__":
     run_assimilator(
         particles_dataset_path="data/advector_output_rivers_2012.nc",
         observations_type="from_csv",
         observations_source_path="data/observations.csv",
-        assimilation_domain_coords=(195 - 360, 23, 235 - 360, 45),
-        assimilation_grid_size=(int(40 / 0.5), int(22 / 0.5)),
+        assimilation_domain_coords=GRID_COORDS,
+        assimilation_grid_size=(int(40 / GRID_RESOLUTION), int(22 / GRID_RESOLUTION)),
         size_ensemble=10,
-        initial_ensemble_spread=0.05,
+        initial_ensemble_spread=50, # Define, a tested, functionning value can be 0.5% of your initial particle mass
         t_start=0,
-        t_end=300,
-        radius_observation=50,
+        t_end=365,
+        radius_observation=np.inf,
+        initial_mass_multiplicator=1000, # Set to your initial particles mass
+        cells_area=cells_area
     )
